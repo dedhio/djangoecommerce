8ac4de93d37bcc3caa813e3a9707678be56f8d1a
geovani <dedhio@gmail.com>
2019-04-05 15:44:35 -0300

    Cria primeiros modelos

---
+++ catalogo/models.py
@@ -0,0 +1,32 @@
+
+from django.db import models
+
+
+class Categoria(models.Model):
+
+    name = models.CharField('Nome', max_length=100)
+    slug = models.SlugField('Identificador', max_length=100)
+
+    created = models.DateTimeField('Criado em', auto_now_add=True)
+    modified = models.DateTimeField('Modificado em', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        ordering = ['name']
+
+
+class Servicos(models.Model):
+    name = models.CharField('Nome', max_length=100)
+    slug = models.SlugField('Identificador', max_length=100)
+    categoria = models.ForeignKey('catalogo.Categoria', on_delete=models.CASCADE)
+    descricao = models.TextField('Descrição', blank=True)
+    preco= models.DecimalField('Preço', decimal_places=2, max_digits=10)
+
+    created = models.DateTimeField('Criado em', auto_now_add=True)
+    modified = models.DateTimeField('Modificado em', auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Serviço'
+        verbose_name_plural = 'Serviços'
+        ordering = ['name']
