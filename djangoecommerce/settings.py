e6e752e3f9d061dcb20cc05e90d7e762629acfad
geovani <dedhio@gmail.com>
2019-04-05 13:13:42 -0300

    Adiciona procfile

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -129,7 +129,7 @@ DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
