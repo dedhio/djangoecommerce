4404ba6457559d5ca8b65b1f2c135a6c62711f9e
geovani <dedhio@gmail.com>
2019-04-05 11:59:00 -0300

    Prepara dependencias e cria testes

---
+++ core/tests/tests_views.py
@@ -0,0 +1,21 @@
+
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class IndexViewTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.client = Client()
+        self.url = reverse('index')
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_status_code(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_status_code(self):
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'index.html')
