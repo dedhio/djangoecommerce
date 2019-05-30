d0abeb4adf1e4e17ec82ad7eb0cd2554c7d7bdd1
Geovani Lopes <dedhio@gmail.com>
2019-05-30 01:01:02 -0300

    Testa alterar dados user

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,5 +1,8 @@
 from django.test import Client, TestCase
 from django.urls import reverse
+from django.conf import settings
+
+from model_mommy import mommy
 
 from accounts.models import User
 
@@ -22,3 +25,24 @@ class RegisterViewTestCase(TestCase):
         response = self.client.post(self.register_url, data)
         self.assertFormError(response, 'form', 'email', 'Este campo é obrigatório.')
 
+
+class UpdateUserTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.client = Client()
+        self.url = reverse('accounts:atualiza_user')
+        self.user = mommy.prepare(settings.AUTH_USER_MODEL)
+        self.user.set_password('123')
+        self.user.save()
+
+    def test_update_user_ok(self):
+        data = {'name': 'test', 'email': 'test@test.com'}
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 302)
+        self.client.login(username=self.user.username, password='123')
+        response = self.client.post(self.url, data)
+        accounts_index_url = reverse('accounts:minha_conta')
+        self.assertRedirects(response, accounts_index_url)
+        self.user.refresh_from_db()
+        self.assertEquals(self.user.email, 'test@test.com')
+        self.assertEquals(self.user.name, 'test')
